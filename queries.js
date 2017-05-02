40fb10c80d532d1fae03013df6b7666bda415fec
maubarrerag <mauricio.barrerag@gmail.com>
2017-05-01 19:43:05 -0500

    Update queries

--- queries.js
+++ queries.js
@@ -29,7 +29,19 @@ function getAllRecords(req, res, next){
 }
 
 function createEmRecord(req, res, next){
-
+	req.body.zone = parseInt(req.body.zone);
+	req.body.issue = parseInt(req.body.issue);
+	var date = new Date();
+	db.none('insert into emrecord (data) values($1)',JSON.stringify({"zone":req.body.zone,
+		"issue":req.body.issue,"comments":req.body.comments,"date": date})).
+	then(function(){
+		res.status(200).json({
+			status: 'success',
+			message: 'Added succesfully'
+		});
+	}).catch(function(err){
+		return next(err);
+	});
 }
 
 function updateEmRecord(req, res, next){
@@ -37,7 +49,21 @@ function updateEmRecord(req, res, next){
 }
 
 function getEmRecord(req, res, next){
-
+	var id = parseInt(req.params.id);
+	db.one('select * from emrecord where id = $1', id).then(function(data){
+		res.status(200).json({
+			status: 'success',
+			data: data,
+			message: 'OK'
+		});
+	}).catch(function(err){
+		res.status(300).json({
+			status: 'error',
+			data: null,
+			message: "Imposible to get information from emrecord:"+id
+		});
+		return 0;
+	});
 }
 
 function removeEmRecord(req, res, next){
